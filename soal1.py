b334847ea1b4167bd81dfef792ae42b44bc96811
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-09-22 16:10:09 +0700

    jawaban

---
+++ soal1.py
@@ -0,0 +1,17 @@
+percen = float(input("Masukkan Nilai Persentase: "))
+
+if percen >= 90:
+    print("Sangat baik")
+elif percen >= 80:
+    print("Baik")
+elif percen >= 70:
+    print("Cukup baik")
+elif percen >= 60:
+    print("Cukup")
+else:
+    print("Tidak baik")
+
+
+
+
+
